160df5e47411a2cdd3332dedc4cfb5ac79428890
jcha2 <jcha2@naver.com>
2021-08-24 10:16:40 +0900

    Added method to check peak if it can be interpreted as reporter ion

--- src/exp/data/Peak.java
+++ src/exp/data/Peak.java
@@ -1,32 +1,51 @@
 
 package exp.data;
+
+import exp.statics.IsobaricTag;
+
 /**
  * Store one peak information of a spectrum
  */
 public class Peak {
 	public double mass;
 	public double intensity;
-	
+
 	/**
 	 * Get peak mass value 
 	 * @return mass 
 	 */
-//	public double getMass() {
-//		return mass;
-//	}
-	
-//	protected void setMass(double mass) {
-//		this.mass = mass;
-//	}
+	//	public double getMass() {
+	//		return mass;
+	//	}
+
+	//	protected void setMass(double mass) {
+	//		this.mass = mass;
+	//	}
 	/**
 	 * Get peak intensity value 
 	 * @return intensity 
 	 */
-//	public double getIntensity() {
-//		return intensity;
-//	}
-//	protected void setIntensity(double intensity) {
-//		this.intensity = intensity;
-//	}
-}
+	//	public double getIntensity() {
+	//		return intensity;
+	//	}
+	//	protected void setIntensity(double intensity) {
+	//		this.intensity = intensity;
+	//	}
 
+	/**
+	 * return whether this peak can be interpreted as Reporter ion or not
+	 * 
+	 * @param label protocol index, fragment tolerance
+	 * @return boolean (true = thought as reporter ion)
+	 */
+	public boolean checkReporterIon(int label_protocol, double frag_tol) {
+		double reporterIonMasses[] = IsobaricTag.MASS_TABLE[label_protocol];
+		for (int index = 1; index < reporterIonMasses.length; index++) {
+			if (this.mass - frag_tol <= reporterIonMasses[index] && this.mass + frag_tol >= reporterIonMasses[index]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
