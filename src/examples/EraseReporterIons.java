d349d01561fdbcfb90d23bc2a2fee8f6e62371d5
jcha2 <jcha2@naver.com>
2021-08-24 10:17:57 +0900

    Erased methods and Added methods to Spectra & Peak class

--- src/examples/EraseReporterIons.java
+++ src/examples/EraseReporterIons.java
@@ -27,13 +27,10 @@ public class EraseReporterIons {
 	 **/
 
 	// input mgf dir path, output mgf dir path
-	public static final String INPUTMGFPATH = "D:\\Data\\ProteomeTools\\MGF\\1\\";
-	public static final String OUTPUTMGFPATH = "D:\\Data\\ProteomeTools\\MGF\\RPErasedMGF";
-
-	// label protocol : 0 = iTRAQ 4plex, 1 = iTRAQ 8plex, 2 = TMT 2plex, 3 = TMT 6plex, 4 = TMT 10plex 
-	public static final int LABELPROTOCOL = 4;
-	// Fragment tolerance
-	public static final double FRAG_TOL = 0.025;
+	public static final String INPUTMGFPATH = "D:\\Data\\ProteomeTools\\mgf\\1\\";
+	public static final String OUTPUTMGFPATH = "D:\\Data\\ProteomeTools\\mgf\\RPerasedMGF";
+	public static final int LABELPROTOCOL = 4; // label protocol : 0 = iTRAQ 4plex, 1 = iTRAQ 8plex, 2 = TMT 2plex, 3 = TMT 6plex, 4 = TMT 10plex 
+	public static final double FRAG_TOL = 0.025; // Fragment tolerance
 
 	public static void main(String[] args) throws IOException {
 		File input_dir = new File(INPUTMGFPATH);
@@ -41,17 +38,18 @@ public class EraseReporterIons {
 		if (!output_dir.exists())
 			output_dir.mkdir();
 
-		// reading mgf file one by one
+		// read mgf file one by one
 		File input_mgfs[] = input_dir.listFiles();
 		for (File input_mgf : input_mgfs) {
-			//saving all spectra
+			//save all spectra
 			Spectra spectra = new Spectra();
 			spectra.readFiles(input_mgf.getAbsolutePath());
+			spectra = spectra.getReporterIonErasedSpectra(LABELPROTOCOL, FRAG_TOL);
 
 			// get new mgf path
 			String title = input_mgf.getName().replace(".mgf", "");
 			File newmgf = new File(OUTPUTMGFPATH + File.separatorChar + title + "_RIonErased.mgf");
-			System.out.println(newmgf.getAbsolutePath());
+			System.out.println("Now working on: " + newmgf.getAbsolutePath());
 			BufferedWriter newmgf_bw = new BufferedWriter(new FileWriter(newmgf));
 
 			// write new mgf
@@ -64,33 +62,11 @@ public class EraseReporterIons {
 				newmgf_bw.write("PEPMASS=" + ions.getPepMassInfo() + "\n");
 				newmgf_bw.write("CHARGE=" + ions.getTitle().split("\\.")[3] + "+\n");
 
-				//erase reporter ion mass w/ peak info and reporterion mass table
-				List<Peak> peaks = ions.getPeakinfo();
-				for (Peak peak : peaks) {
-					boolean checkion = CheckReporterIon(peak.mass);
-					if (!checkion)
-						newmgf_bw.write(peak.mass + " " + peak.intensity + "\n");
-					else
-						continue;
-				}
+				for (Peak peak : ions.getPeakinfo())
+					newmgf_bw.write(peak.mass + " " + peak.intensity + "\n");
 				newmgf_bw.write("END IONS\n");
 			}
 			newmgf_bw.close();
 		}
-	}
-
-	/** 
-	 * Check if the mass is within reporter ion mass +- fragment tol
-	 * @param mass - peak mass
-	 * @return boolean 
-	 */
-	private static boolean CheckReporterIon(double mass) {
-		double reporterIonMasses[] = IsobaricTag.MASS_TABLE[LABELPROTOCOL];
-		for (int index = 1; index < reporterIonMasses.length; index++) {
-			if (mass - FRAG_TOL <= reporterIonMasses[index] && mass + FRAG_TOL >= reporterIonMasses[index]) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
